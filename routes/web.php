cb898bffd4b1e727eb54f2569c9d24e30c63355f
Nikita-kk <nikitakhanorkar989@gmail.com>
2023-04-04 16:57:31 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\CreateController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('layouts.master',[CreateController::class,'master'])->name('layouts.master');
+Route::get('table',[CreateController::class,'table'])->name('table');
